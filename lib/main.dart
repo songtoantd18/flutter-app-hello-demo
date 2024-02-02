8bdc12eaa002ad1d0e28fe3799a924333a6fd9dd
songtoantd18@gmail.com <songtoan>
2024-02-02 16:49:53 +0700

    flutterNotificationFirebase3.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:app_hello/home_page.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //initialize firebase from firebase core plugin
   await Firebase.initializeApp();
   runApp(MyApp(homeScreen: HomePage()));
 }
@@ -23,5 +23,103 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       home: this.widget.homeScreen,
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+  init() async {
+    String deviceToken = await getDeviceToken();
+    print("###### PRINT DEVICE TOKEN TO USE FOR PUSH NOTIFICATION ######");
+    print(
+        'đây là token --------------------------------------------------$deviceToken');
+    print("############################################################");
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage remoteMessage) {
+      String? title = remoteMessage.notification!.title;
+      String? description = remoteMessage.notification!.body;
+
+      Alert(
+        context: context,
+        type: AlertType.error,
+        title: title,
+        desc: description,
+        buttons: [
+          DialogButton(
+            child: Text(
+              "COOL",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: () => Navigator.pop(context),
+            width: 120,
+          )
+        ],
+      ).show();
+    });
+
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    // Rest of your initialization code
+  }
+
+  Future<void> _firebaseMessagingBackgroundHandler(
+      RemoteMessage message) async {
+    print("Handling a background message: ${message.messageId}");
+    // Handle the background message here
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Text(
+                      "Firebase Messaging",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Text(
+                      "How to integrate Firebase Messaging",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future getDeviceToken() async {
+    FirebaseMessaging.instance.requestPermission();
+    FirebaseMessaging _firebaseMessage = FirebaseMessaging.instance;
+    String? deviceToken = await _firebaseMessage.getToken();
+
+    return (deviceToken == null) ? "" : deviceToken;
   }
 }
